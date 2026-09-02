1c5a72f05177c5b92337964da9195ae844650e46
s0um1kx <soumik.workmail@gmail.com>
2026-09-02 16:07:10 +0530

    Task2 done

---
+++ Task2.c
@@ -0,0 +1,14 @@
+//cube of a number 
+#include <stdio.h>
+
+int main() {
+    int num, cube;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    cube = num * num * num;
+    printf("Cube of %d is %d\n", num, cube);
+
+    return 0;
+}
